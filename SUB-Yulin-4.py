1a0b59b6542c340058f3883ad05cce3d72c81742
DueiHowLightAction <a0956638806@gmail.com>
2024-05-31 00:02:12 +0800

    1. No pumping in the first step. 2. Plot the compaction change with time with three layers, respectively.

--- SUB-Yulin-4.py
+++ SUB-Yulin-4.py
@@ -15,13 +15,13 @@ path = os.path.join("git")
 
 ##### Creating the MODFLOW Model #####
 temp_dir = TemporaryDirectory()
-workspace = Path(r"D:\NTU_Stride-C\codes\modflow\git")
+workspace = Path(r"D:\NTU_Stride-C\codes\modflow\workspace")
 name = "SUB-Yulin-5"
 mf = flopy.modflow.Modflow(name, exe_name="mf2005", model_ws=workspace)
 
 # DIS
 Lx = 25000        #25870
-Ly = 20
+Ly = 50
 nlay = 3
 ncol = 100
 nrow = 1
@@ -66,6 +66,10 @@ ibound[0, :, :] = 2
 ibound[0, :, int(ncol-1)] = -1
 ibound[0, :, 0] = -1
 strt = 10.0*np.ones((nlay, nrow, ncol),dtype=np.float32)
+for i in range(ncol):
+    strt[0, :, i] = (-zbot[0, :, i]-0)/2
+    strt[0, :, i] = (-zbot[1, :, i]+zbot[0, :, i])/2
+    strt[0, :, i] = (-zbot[2, :, i]+zbot[1, :, i])/2
 #strt[0, :, :] = zbot[0, :, 0]
 #strt[1, :, :] = zbot[1, :, 0]
 #strt[2, :, :] = zbot[2, :, 0]
@@ -102,14 +106,9 @@ bcf = flopy.modflow.ModflowBcf(
 pumping_rate = -1.53169e5
 recharging_rate = 100
 wel_sp = np.zeros((1, 4))
-#wel_sp[:, 0] = 1
-#wel_sp[:, 1] = 1
-#wel_sp[:, 3] = recharging_rate
-wel_sp[0, 3] = pumping_rate
-#wel_spncol//2-1 + ncol, 3] = pumping_rate
 wel_sp[0, 2] = ncol/2-1
-
-stress_period_data = {i: wel_sp for i in range(nper)}
+wel_sp[0, 3] = pumping_rate
+stress_period_data = {i: wel_sp for i in range(1, nper)}
 wel = flopy.modflow.ModflowWel(
     mf, 
     stress_period_data=stress_period_data
@@ -238,69 +237,118 @@ assert success, "MODFLOW did not terminate normally."
 # which could be manipulated (or used) later if necessary.
 
 # plotting B.C. (iBound) (well) 
-fig = plt.figure(figsize=(15, 5))
-ax = fig.add_subplot(1, 1, 1)
 
-xsect = flopy.plot.PlotCrossSection(model=mf, line={"Row": 0})
-patches = xsect.plot_bc("WEL", color="pink")
-patches = xsect.plot_ibound()
-linecollection = xsect.plot_grid()
-t = ax.set_title("XZ Cross-Section with Boundary Conditions")
+#ax = fig.add_subplot(1, 1, 1)
 
-workspace1 = Path(r"D:\NTU_Stride-C\codes\modflow\SUB-Yulin-3\SUB-Yulin-3")
+#xsect = flopy.plot.PlotCrossSection(model=mf, line={"Row": 0})
+
+#t = ax.set_title("XZ Cross-Section with Boundary Conditions")
+
+workspace1 = Path(r"D:\NTU_Stride-C\codes\modflow\workspace\SUB-Yulin-5")
 
 # 地層下陷量的二進位檔轉ASCII
-sobj = bf.HeadFile(r"D:\NTU_Stride-C\codes\modflow\SUB-Yulin-3\SUB-Yulin-3.subsidence.hds",
+sobj = bf.HeadFile(r"D:\NTU_Stride-C\codes\modflow\workspace\SUB-Yulin-5.subsidence.hds",
                    text="SUBSIDENCE")
-sobj1 = bf.HeadFile(r"D:\NTU_Stride-C\codes\modflow\SUB-Yulin-3\SUB-Yulin-3.total_comp.hds",
+sobj1 = bf.HeadFile(r"D:\NTU_Stride-C\codes\modflow\workspace\SUB-Yulin-5.total_comp.hds",
                    text="COMPACTION")
 sobj2 = bf.HeadFile(workspace1.with_suffix(".hds"),)
 
 times = sobj.get_times()
-#times_day = times/365
+print(times)
 subs = sobj.get_data(totim=times[1])
-subs_t = np.zeros(len(times))
+subs_t = np.zeros((len(times), ncol))
+comp1_t = np.zeros((len(times), ncol))
+comp2_t = np.zeros((len(times), ncol))
+comp3_t = np.zeros((len(times), ncol))
 print(len(times))
 for i in range(len(times)):
-    subs_t[i] = sobj.get_data(totim=times[i])[0][0][0]
-times1 = sobj1.get_times()
+    for j in range(ncol):
+        subs_t[i, j] = sobj.get_data(totim=times[i])[0,0,j]
+        comp1_t[i, j] = sobj1.get_data(totim=times[i])[0,0,j]
+        comp2_t[i, j] = sobj1.get_data(totim=times[i])[1,0,j]
+        comp3_t[i, j] = sobj1.get_data(totim=times[i])[2,0,j]
+
+       
 comp = sobj1.get_data()
-times2 = sobj2.get_times()
-head = sobj2.get_data(totim=times2[1])
+head = sobj2.get_data(totim=times[1])
 # v_disp是3d array: [[[a, b, c]]]，需要取其中一個列
 np.savetxt(workspace1.with_name("subs.txt"), subs[0])
+np.savetxt(workspace1.with_name("subs_t.txt"), subs_t[:])
+np.savetxt(workspace1.with_name("compl1_t.txt"), comp1_t[:])
+np.savetxt(workspace1.with_name("compl2_t.txt"), comp2_t[:])
 np.savetxt(workspace1.with_name("compl1.txt"), comp[0])
 np.savetxt(workspace1.with_name("compl2.txt"), comp[1])
 np.savetxt(workspace1.with_name("compl3.txt"), comp[2])
 np.savetxt(workspace1.with_name("head.txt"), head[0])
 
-fig, (ax1, ax2) = plt.subplots(2, 1)#, figsize=(18, 5))
 
-# 畫出各層的水頭值
-extent = (delr / 2.0, Lx - delr / 2.0, Ly - delc / 2.0, delc / 2.0)
+# col 1 的水頭剖面隨時間變化
+#plt.title("Heads of vertical section in row 1")
+fig = plt.figure(figsize=(15, 9))
+plot_kper=[0, 5, 10]        # 這樣設是為了下面畫well profile
+mytimes = [times[plot_kper[0]], times[plot_kper[1]], times[plot_kper[2]]]
+for iplot, time in enumerate(mytimes):
+    print("*****Processing time: ", time)
+    head = sobj2.get_data(totim=time)
+    # Print statistics
+    print("Head statistics")
+    print("  min: ", head.min())
+    print("  max: ", head.max())
+    print("  std: ", head.std())
+
+    ax = fig.add_subplot(len(mytimes)+1, 1, iplot + 1, aspect="equal")
+    ax.set_title(f"day {time}")
+
+    ysect = flopy.plot.PlotCrossSection(model=mf, line={"Row": 0})
+    pc = ysect.plot_array(head, head=head, alpha=0.5, ax=ax)
+    pc1 = ysect.contour_array(head, head=head, alpha=0.5, ax=ax)#, levels=np.arange(1, 10, 1))
+    patches = ysect.plot_ibound(head=head, ax=ax)
+    linecollection = ysect.plot_grid(ax=ax)
+    cb = plt.colorbar(pc, shrink=0.75, ax=ax)
+
+    patches = ysect.plot_bc("WEL", kper=plot_kper[iplot], color="pink")
+    patches = ysect.plot_ibound()
+    linecollection = ysect.plot_grid()
+
+plt.show()
+
 
 fig = plt.figure(figsize=(10, 6))
-gs = gridspec.GridSpec(2, 3, height_ratios=[2, 1])
-ax1 = plt.subplot(gs[0, 0])
-ax2 = plt.subplot(gs[0, 1])
-ax3 = plt.subplot(gs[0, 2])
-ax4 = plt.subplot(gs[1, :])
 
-#ax1.contourf(head[0, :, :], levels=np.arange(1, 23, 1), extent=extent)
-ax1.set_title("layer 1 head")
-#ax2.contourf(head[1, :, :], levels=np.arange(1, 23, 1), extent=extent)
-ax2.set_title("layer 2 head")
-#ax3.contourf(head[2, :, :], levels=np.arange(1, 23, 1), extent=extent)
-ax3.set_title("layer 3 head")
-ax4.plot(times, subs_t)
-ax4.set_xlabel("time (days)")
-ax4.set_ylabel("subsidence(m)")
 
-# 調整子圖之間的間距
-plt.tight_layout()
+plt.plot(times, subs_t[:,50], "-o")
+plt.plot(times, comp1_t[:,50], "-o")
+plt.plot(times, comp2_t[:,50], "-o")
+plt.plot(times, comp3_t[:,50], "-o")
+plt.xlabel("time (days)")
+plt.ylabel("subsidence(m)")
 
-# 顯示圖形
 plt.show()
+
+# 畫出各層的水頭值
+#extent = (delr / 2.0, Lx - delr / 2.0, Ly - delc / 2.0, delc / 2.0)
+
+#fig = plt.figure(figsize=(10, 6))
+#gs = gridspec.GridSpec(2, 3, height_ratios=[2, 1])
+#ax1 = plt.subplot(gs[0, 0])
+#ax2 = plt.subplot(gs[0, 1])
+#ax3 = plt.subplot(gs[0, 2])
+#ax4 = plt.subplot(gs[1, :])
+#
+##ax1.contourf(head[0, :, :], levels=np.arange(1, 23, 1), extent=extent)
+#ax1.set_title("layer 1 head")
+##ax2.contourf(head[1, :, :], levels=np.arange(1, 23, 1), extent=extent)
+#ax2.set_title("layer 2 head")
+##ax3.contourf(head[2, :, :], levels=np.arange(1, 23, 1), extent=extent)
+#ax3.set_title("layer 3 head")
+#ax4.plot(times, subs_t)
+#ax4.set_xlabel("time (days)")
+#ax4.set_ylabel("subsidence(m)")
+
+# 調整圖之間的間距
+#plt.tight_layout()
+
+
 
 try:
     temp_dir.cleanup()
